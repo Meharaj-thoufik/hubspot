e5b9acf61f3a6cf2c8612fda13e94bba8b379e7f
Meharaj Thoufik <meharaj.thoufik@lmnas.com>
2025-05-21 10:56:33 +0530

    graphql in production

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+// export default () => ({});
+
+
+module.exports = {
+  graphql: {
+    config: {
+      landingPage: true, // Enables the Apollo Sandbox in production
+    },
+  },
+};
